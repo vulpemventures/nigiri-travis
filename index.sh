7aaca869c75d2972d7d7c30f3cca4d518071cfdd
Marco Argentieri <marco@vulpem.com>
2020-03-04 23:08:08 +0100

    Add script; Remove env variables
    Signed-off-by: Marco Argentieri <marco@vulpem.com>

---
+++ index.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+##/=====================================\
+##|    Creating directories     |
+##\=====================================/
+mkdir -p config liquid-config
+chmod -R 755 .
+
+ 
+##/=====================================\
+##|    Download conf files   |
+##\=====================================/
+curl -o ./config/bitcoin.conf -sL https://raw.githubusercontent.com/vulpemventures/nigiri-travis/master/bitcoin.conf
+curl -o ./liquid-config/bitcoin.conf -sL https://raw.githubusercontent.com/vulpemventures/nigiri-travis/master/elements.conf
+
+##/=====================================\
+##|    Download compose   |
+##\=====================================/
+curl -o docker-compose.yml -sL https://raw.githubusercontent.com/vulpemventures/nigiri-travis/master/docker-compose.yml
+
